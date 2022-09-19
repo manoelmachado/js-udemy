40ad7a29e690fed77985ebde07a7f251b4d3ab65
manoelmachado <m.machado49@hotmail.com>
2022-09-19 16:32:51 -0300

    valores primitivos e por referencia

---
+++ valores primitivos e por referencia/script.js
@@ -0,0 +1,57 @@
+/*
+let nome = 'Luiz';
+nome = 'Otávio';
+console.log(nome)
+*/
+
+
+/*
+let a = 'A';
+let b = a;
+console.log(a, b);
+
+a = 'Outra coisa';
+console.log (a, b);
+*/
+
+/*
+let a = [1, 2, 3];
+let b = a;
+console.log(a, b);
+a.push(4);
+console.log(a, b);
+b.pop();
+console.log(a, b);
+*/
+
+
+/*
+let a = [1, 2, 3];
+let b = [...a];
+console.log(a, b)
+a.push(4)
+console.log(a, b);
+*/
+
+
+/*
+const a = { 
+    nome: 'Luiz',
+    sobrenome: 'otávio'
+}; 
+const b = a 
+a.nome = 'João'
+console.log(b);
+*/
+
+
+/*
+const a = {
+    nome: 'Luiz',
+    sobrenome: 'otávio'
+};
+const b = {...a}
+a.nome = 'João';
+console.log(b);
+console.log(a)
+*/
