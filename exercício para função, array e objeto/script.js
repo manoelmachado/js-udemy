2b61c4f1a9f1cc4e01672ffedaef4c0478c2ba71
manoelmachado <m.machado49@hotmail.com>
2022-09-20 13:29:42 -0300

    Update script.js

--- exercício para função, array e objeto/script.js
+++ exercício para função, array e objeto/script.js
@@ -2,27 +2,20 @@ function meuEscopo() {
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
     const pessoas = [];
-
     function recebeEventoForm (evento) {
         evento.prefentDefault();
-
         const nome = form.querySelector('.nome');
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
-
         pessoas.push({
             nome: nome.value,
             sobrenone: sobrenome.value,
             peso: peso.value,
             altura: altura.value
         });
-
         resultado.innerHTML += `<p>${none.value}, ${sobrenone.value} ` + ` ${peso.value}, ${altura.value}</p>`;
     }
-
-    
-
-    form.addEventListener('submit', recebeEventoForm);
+     form.addEventListener('submit', recebeEventoForm);
 }
 meuEscopo();
