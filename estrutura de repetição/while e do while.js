6a199704f5590199176b9897c253c8b8643c7e4c
manoelmachado <m.machado49@hotmail.com>
2022-11-21 17:02:30 -0300

    1

--- estrutura de repetição/while e do while.js
+++ estrutura de repetição/while e do while.js
@@ -1,5 +1,26 @@
+/*
 let i = 0
 while (i < 10) {
     console.log(`O número é ` + i)
     i++;
+} 
+*/
+
+// CRIANDO UMA FUNÇÃO PARA GERAR UM NÚMERO ALEATÓRIO DE 1 ATÉ 50
+/*
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
 }
+
+const min = 1;
+const max = 50;
+let rand = random(min, max);
+
+while (rand !== 10) {
+    rand = random(min, max);
+    console.log(rand);
+}
+*/
+// ENQUANTO O NÚMERO DA MINHA FUNÇÃO random(min, max) NÃO FOR 10, RETORNAR O NÚMERO GERADO ALEATORIAMENTE
+
