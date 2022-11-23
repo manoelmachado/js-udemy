1e5db56fc1fb490af245d1ac47d009433864f596
manoelmachado <m.machado49@hotmail.com>
2022-11-23 13:13:29 -0300

    1

--- exercício com lógica de programação/ex03.js
+++ exercício com lógica de programação/ex03.js
@@ -1,18 +1,6 @@
 // while num <= 100
 
 /*
-função fizzBuzz(numero) {
-    let numero = 0
-    enquanto numero <= 100{
-
-    }
-}
-
-console.log(fizzBuzz(numero))
-
-*/
-
-/*
 function fizzBuzz() {
     var num = 0
     while (num <= 100) {
@@ -34,12 +22,12 @@ console.log(fizzBuzz())
 
 function fizzBuzz(numero) {
     if (typeof numero !== 'number') return numero;
-    if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
+    if (numero % 15 === 0) return 'FizzBuzz';
     if (numero % 3 === 0) return 'Fizz';
     if (numero % 5 === 0) return 'Buzz';
     return numero;
 }
 
-for (let i = 0; 1 <= 100; i++) {
+for (let i = 0; i <= 100; i++) {
     console.log(i, fizzBuzz(i))
 }
